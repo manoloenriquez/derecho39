661995f82820d1266128d0cd5ce2e16cef62b310
Matthew Escober <escobermatthew1029@gmail.com>
2024-11-06 20:04:35 +0800

    nav bar stuff and the startup
    fuck this shit

---
+++ derecho-39/src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Derecho Santugon</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/eb">Executive Board</a>
+                <a href="/colleges">Colleges</a>
+                <a href="/la">LA Agenda</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
