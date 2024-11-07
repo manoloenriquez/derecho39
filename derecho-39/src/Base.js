0b4beeee1cf55607531849d4bad5e843678270be
Matthew Escober <escobermatthew1029@gmail.com>
2024-11-08 02:27:45 +0800

    base stuff
    ayoko na

--- derecho-39/src/Base.js
+++ derecho-39/src/Base.js
@@ -3,10 +3,12 @@ const Base = () => {
         <div className="base">
             <div className="eb">
                 <div className="vision">
-                    <img src="/block1.svg" alt="rando circles" className="block1" />
-                    <h1 className="vis1">REALIZED</h1>
-                    <h1 className="vis1">LASALLIAN</h1>
-                    <h1 className="vis2">PURPOSE</h1>
+                    <div className="visionWords">
+                        <img src="/block1.svg" className="block1" width="100px"/>   
+                        <h1 className="vis1">REALIZED</h1>
+                        <h1 className="vis1">LASALLIAN</h1>
+                        <h1 className="vis2">PURPOSE</h1>
+                    </div>
                 </div>
                 <div className="eb-column">
                     <img src="/base-eb.png" alt="EB base pic" className="baseEb" />
