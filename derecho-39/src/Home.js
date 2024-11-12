68bff85b5c936949c9195c6fba9337276f10ebfa
mattescober <145801129+matt3scober@users.noreply.github.com>
2024-11-12 10:19:58 +0800

    home done
    yeah

--- derecho-39/src/Home.js
+++ derecho-39/src/Home.js
@@ -1,31 +1,43 @@
+// Home.js
 import { Link } from 'react-router-dom';
+import './home.css';
+
+const images = [
+  { src: '/ccs-slate.png', alt: 'CCS', link: '/ccs' },
+  { src: '/cla-slate.png', alt: 'CLA', link: '/cla' },
+  { src: '/cos-slate.png', alt: 'COS', link: '/cos' },
+  { src: '/cob-slate.png', alt: 'RVR-COB', link: '/rvr-cob' },
+  { src: '/soe-slate.png', alt: 'SOE', link: '/soe' },
+];
 
 const Home = () => {
-    return (  
-        <div className="home">
-            <div className="collegesFlex">
-                <div className="threeC">
-                    <Link to="/colleges">
-                        <img src="/ccs-slate.png" alt="ccs slate pic" className='ccsGrid'/>
-                    </Link>
-                    <Link to="/colleges">
-                        <img src="/cla-slate.png" alt="cla slate pic" className='claGrid'/>
-                    </Link>
-                    <Link to="/colleges">
-                        <img src="/cos-slate.png" alt="cos slate pic" className='cosGrid'/>
-                    </Link>
-                </div>
-                <div className="twoC">
-                    <Link to="/colleges">
-                        <img src="/cob-slate.png" alt="cob slate pic" className='cobGrid'/>
-                    </Link>
-                    <Link to="/colleges">
-                        <img src="/soe-slate.png" alt="soe slate pic" className='soeGrid'/>
-                    </Link>
-                </div>
+  return (
+    <div className="home">
+      <div className="image-container">
+        {/* First row with three images */}
+        <div className="image-row">
+          {images.slice(0, 3).map((image, index) => (
+            <div className="image-wrapper" key={index}>
+              <Link to={image.link}>
+                <img src={image.src} alt={image.alt} className="group-image" />
+              </Link>
             </div>
+          ))}
         </div>
-    );
-}
- 
+
+        {/* Second row with two centered images */}
+        <div className="image-row" style={{ justifyContent: 'center' }}>
+          {images.slice(3).map((image, index) => (
+            <div className="image-wrapper" key={index}>
+              <Link to={image.link}>
+                <img src={image.src} alt={image.alt} className="group-image" />
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
